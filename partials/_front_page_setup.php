c6c782ad103eaba19af7039c9e7714ee66d125a6
radius314 <radius314@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-02-24 02:29:37 +0000

    version 1.0.5
    git-svn-id: https://plugins.svn.wordpress.org/bread/trunk@1828114 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- partials/_front_page_setup.php
+++ partials/_front_page_setup.php
@@ -18,7 +18,7 @@ if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
                     <p>Default Font Size: <input min="4" max="18" step=".1" size="3" maxlength="3" type="number" class="bmlt-input-field" style="display:inline;" id="front_page_font_size" name="front_page_font_size" value="<?php echo esc_html($this->options['front_page_font_size']) ;?>" />&nbsp;&nbsp;
                     Line Height: <input min="1" max="3" step=".1" size="3" maxlength="3" type="number" class="bmlt-input-field" style="display:inline;" id="front_page_line_height" type="text" maxlength="3" size="3" name="front_page_line_height" value="<?php echo esc_html($this->options['front_page_line_height']) ;?>" /></p>
                     <div style="margin-top:15px; margin-bottom:20px; max-width:100%; width:100%;">
-                        <?
+                        <?php
                         $editor_id = "front_page_content";
                         $settings    = array (
                             'tabindex'      => FALSE,
