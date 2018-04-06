c551b3441ff0dc882c4b151cf6191667fc60d319
Kevin Grimley <klgrimley@users.noreply.github.com>
2018-04-06 07:22:18 -0400

    Update to https
    Changed youTube source from http to https

--- partials/_help_videos.php
+++ partials/_help_videos.php
@@ -15,7 +15,7 @@
 <div class="help-video" id="current-meeting-list-video" style="overflow: hidden !important;display:none; height:650px !important; width:900px !important;">
     <span class="b-close"><span>X</span></span>
     <video preload="none" id="myVideo" width="900" height="650" controls="controls">
-        <source type="video/youtube" src="http://youtu.be/qG5Iu1vtCU0?vq=hd1080" />
+        <source type="video/youtube" src="https://www.youtube.com/watch?v=qG5Iu1vtCU0&feature=youtu.be&vq=hd1080" />
         Your browser does not support HTML5 video.
     </video>
 </div>
