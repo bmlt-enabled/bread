ec6cd7d0c450f15e38bb23f48456839da22e224e
Danny Gershman <danny.gershman@gmail.com>
2018-10-28 00:57:40 -0400

    version bump

--- bmlt-meeting-list.php
+++ bmlt-meeting-list.php
@@ -4,7 +4,7 @@ Plugin Name: bread
 Plugin URI: http://wordpress.org/extend/plugins/bread/
 Description: Maintains and generates a PDF Meeting List from BMLT.
 Author: odathp, radius314, pjaudiomv, klgrimley
-Version: 1.7.2
+Version: 1.7.3
 */
 /* Disallow direct access to the plugin file */
 use Mpdf\Mpdf;
---
+++ bread.php
@@ -0,0 +1,1 @@
+<?php/** * Do not modify the files in this folder. */?>
