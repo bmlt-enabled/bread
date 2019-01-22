3fbefb2221c4442cf4645a2f5cab88d64b4b54d8
Danny Gershman <danny.gershman@gmail.com>
2019-01-22 00:51:08 -0500

    removing heavy fonts that are unused

---
+++ simplify-mpdf.sh
@@ -0,0 +1,4 @@
+rm mpdf/vendor/mpdf/mpdf/ttfonts/Sun-ExtA.ttf
+rm mpdf/vendor/mpdf/mpdf/ttfonts/Sun-ExtB.ttf
+rm mpdf/vendor/mpdf/mpdf/ttfonts/Aegyptus.otf
+rm mpdf/vendor/mpdf/mpdf/ttfonts/UnBatang_0613.ttf
