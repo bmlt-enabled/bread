bb8e985a5c006a21dd6c464485e6dfedd4955227
Ron Barack <ron.barack@sap.com>
2019-06-06 11:14:39 +0200

    Trying to rebase

--- bmlt-meeting-list.php
+++ bmlt-meeting-list.php
@@ -5,7 +5,7 @@ Plugin URI: http://wordpress.org/extend/plugins/bread/
 Description: Maintains and generates a PDF Meeting List from BMLT.
 Author: bmlt-enabled
 Author URI: https://bmlt.app
-Version: 1.10.0
+Version: 1.10.1
 */
 /* Disallow direct access to the plugin file */
 use Mpdf\Mpdf;
