c38da62dbd1fe96bf5ba03cb7d68784c499feb1c
otrok7 <50595291+otrok7@users.noreply.github.com>
2022-11-07 15:41:39 +0100

    Version Bump

--- bmlt-meeting-list.php
+++ bmlt-meeting-list.php
@@ -5,7 +5,7 @@ Plugin URI: http://wordpress.org/extend/plugins/bread/
 Description: Maintains and generates a PDF Meeting List from BMLT.
 Author: bmlt-enabled
 Author URI: https://bmlt.app
-Version: 2.7.2
+Version: 2.7.3
 */
 /* Disallow direct access to the plugin file */
 use Mpdf\Mpdf;
