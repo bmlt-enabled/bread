3336efa23f16d92a1559321795db4190d06380e9
otrok7 <50595291+otrok7@users.noreply.github.com>
2024-12-17 23:59:19 +0100

    lint

--- admin/class-bread-admin.php
+++ admin/class-bread-admin.php
@@ -447,7 +447,7 @@ class Bread_Admin
     }
     function pwsix_process_wizard()
     {
-        if (isset($_POST['pwsix_wizard_nonce']) || !wp_verify_nonce($_POST['pwsix_wizard_nonce'], 'pwsix_wizard_nonce')) {
+        if (!isset($_POST['pwsix_wizard_nonce']) || !wp_verify_nonce($_POST['pwsix_wizard_nonce'], 'pwsix_wizard_nonce')) {
             return;
         }
         if (!$this->current_user_can_create()) {
